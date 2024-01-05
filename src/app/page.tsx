8e796b5e2fc9c7a19617fb92f44896551e9e62bb
Sandeep Khariwal <sandeepkharival123@gmail.com>
2024-01-05 19:33:16 +0530

    landing page code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,40 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import './page.scss'
+import { TbCarCrane,TbCrane } from "react-icons/tb";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <header>
+      <div className='nav'>
+        <p><TbCarCrane /></p>
+        <h3>KRISHANA CRANE SERVICE, <span>SIRSA</span></h3>
+        <p><TbCrane /></p>
+      </div>
+      <div className='details'>
+      <div className='pic' >
+          <img src='/mamaji.png' alt='Not Found'/>
+        </div>
+        <div className='details' >
+          <p>Krishan Sekhawat</p>
+          <div className='buttons' >
+          <a href="tel:+919729805441" >Call Now</a>
+          <a href="https://wa.me/+919729805441">WhatsApp</a>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='provide' >
+        <h2>We are Available</h2>
+        <ul>
+          <li>Loading upto 20 ton </li>
+          <li>Available in Haryana</li>
+          <li>Available to visit</li>
+          <li>Available 12 * 7</li>
+          <li>Available for rent</li>
+        </ul>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='crane' >
+       <img src='/crane.jpg' alt='Not Found'/>
       </div>
-    </main>
+    </header>
   )
 }
